bb668be9ec8d313ec25e05860af0cd8119725e83
RizkiIqbal02 <roxashiqbal12@gmail.com>
2024-12-01 08:10:21 +0700

    menambahkan tabel kategori dan tabel feedback, serta menyesuaikan tampilan welcome page

---
+++ database/migrations/2024_12_01_002910_create_feedback_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('penduduk_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('kategori_id')->constrained()->cascadeOnDelete();
+            $table->integer('rating');
+            $table->text('komentar');
+            $table->string('gambar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedback');
+    }
+};
