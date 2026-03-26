3531bdbecdcf867ad9ffeeda1acd8f587718763d
mikhsanudin2001 <mikhsanudin2001@gmail.com>
2026-03-26 20:46:11 +0700

    update di web.php

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //$feedback = FeedbackDesa::latest()->get();
+
+    //untuk pagination di welcome.balde.php
     $feedback = FeedbackDesa::latest()->paginate(10);
     return view('welcome', ['feedback' => $feedback]);
 });
-
-// Route::get('/', function () {
-//     return redirect('/penduduk/login');
-// })->name('login');
-
-// Route::get('/penduduk', function () {
-//     dd(auth()->user());
-
-//     return 'hai';
-//     // return view('welcome');
-// })->middleware('auth:penduduk');
